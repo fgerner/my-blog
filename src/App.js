8879bd3b1c89b5d469843a2cdef97bd0fad0fb15
fgerner <fredrik.gerner@qantas.com.au>
2022-08-22 20:32:21 +1000

    added routes

--- src/App.js
+++ src/App.js
@@ -3,15 +3,23 @@ import HomePage from "./pages/HomePage";
 
 import {Route, BrowserRouter as Router, Routes} from "react-router-dom";
 import AboutPage from "./pages/AboutPage";
+import ArticlePage from "./pages/ArticlePage";
+import ArticlesList from "./pages/ArticlesList";
+import NavBar from "./NavBar";
 
 const App = () => {
     return (
         <Router>
             <div className="App">
-                <Routes>
-                    <Route path='/' element={<HomePage/>} />
-                    <Route path='/about' element={<AboutPage/>} exact/>
-                </Routes>
+                <NavBar/>
+                <div className="page-body">
+                    <Routes>
+                        <Route path='/' element={<HomePage/>}/>
+                        <Route path='/about' element={<AboutPage/>} exact/>
+                        <Route path='/article' element={<ArticlePage/>} exact/>
+                        <Route path='/articles-list' element={<ArticlesList/>} exact/>
+                    </Routes>
+                </div>
             </div>
         </Router>
 
---
+++ src/pages/ArticlePage.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+const ArticlePage = () => {
+    return (
+        <h1>Article</h1>
+    )
+}
+export default ArticlePage;
