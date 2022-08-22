0f1558858ceced00a9bbd86ebed3eb1bb6f2dab4
fgerner <fredrik.gerner@qantas.com.au>
2022-08-22 18:48:15 +1000

    first commit

---
+++ src/pages/HomePage.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+const HomePage = () => {
+    return (
+        <>
+            <h1>Hello, world</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad aut deserunt ducimus impedit laboriosam
+                minima
+                repellendus vel. A cum debitis dolorem, esse hic, nesciunt omnis perferendis quisquam, reprehenderit
+                similique
+                velit.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad aut deserunt ducimus impedit laboriosam
+                minima
+                repellendus vel. A cum debitis dolorem, esse hic, nesciunt omnis perferendis quisquam, reprehenderit
+                similique
+                velit.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad aut deserunt ducimus impedit laboriosam
+                minima
+                repellendus vel. A cum debitis dolorem, esse hic, nesciunt omnis perferendis quisquam, reprehenderit
+                similique
+                velit.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad aut deserunt ducimus impedit laboriosam
+                minima
+                repellendus vel. A cum debitis dolorem, esse hic, nesciunt omnis perferendis quisquam, reprehenderit
+                similique
+                velit.</p>
+        </>
+    )
+}
+export default HomePage;
